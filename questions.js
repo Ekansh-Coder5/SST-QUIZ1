46063e99834e37c9646db85e38a6c4d1836fdc4f
Ekansh-Coder5 <78861687+Ekansh-Coder5@users.noreply.github.com>
2021-11-24 16:30:02 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,279 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the Capital of Punjab ?",
+    answer: "Chandigarh",
+    options: [
+      "Ludhiana",
+      "Jalandhar",
+      "Chandigarh",
+      "Moga"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many districts are there in the state ?",
+    answer: "22",
+    options: [
+      "32",
+      "22",
+      "27",
+      "28"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many Legislative Assembly seats are here ?",
+    answer: "117",
+    options: [
+      "117",
+      "121",
+      "134",
+      "75"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is the largest city of Punjab ?",
+    answer: "Ludhiana",
+    options: [
+      "Ludhiana",
+      "Jalandhar",
+      "Gurdaspur",
+      "Patiala"
+    ]
+  },
+    {
+    numb: 5,
+    question: "CM of Punjab (2020)?",
+    answer: "Capt Amarinder Singha",
+    options: [
+      "Capt Amarinder Singha",
+      "Sr Parkask Singh Badal",
+      "Bhagwant Mann",
+      "Sukhbir Singh Badal" 
+    ]
+  },
+  {
+    numb: 6,
+    question: "How many candidates does the state send to the Parliament ?",
+    answer: "13",
+    options: [
+      "13",
+      "14",
+      "16",
+      "21"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the official language of the state ?",
+    answer: "Punjabi",
+    options: [
+      "Punjabi",
+      "Gurmukhi",
+      "Hindi",
+      "English"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the literacy rate of the state ?",
+    answer: "76.68 %",
+    options: [
+      "88.2 %",
+      "76.68 %",
+      "81.98 %",
+      "88.09 %"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the emblem of Punjab ?",
+    answer: "Lion Capital of Ashoka",
+    options: [
+      "Khanda",
+      "Lion Capital of Ashoka",
+      "Eagle",
+      "Tiger"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the state animal of Punjab ?",
+    answer: "Black buck",
+    options: [
+      "Black buck",
+      "Lion",
+      "Tiger",
+      "Eagle"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Punjab is located in ____",
+    answer: "North - west",
+    options: [
+      "North - west",
+      "North - east",
+      "SOUTH- EAST",
+      "SOUTH -WEST"
+    ]
+  },
+  {
+    numb: 12,
+    question: "._____ is the main occupation of the people of Punjab.",
+    answer: "Agriculture",
+    options: [
+      "Fishing",
+      "cattle rearing",
+      "Agriculture",
+      "Carpantery"
+    ]
+  },
+  {
+    numb: 13,
+    question: "What crops are grown in Punjab?  ",
+    answer: "all of the above",
+    options: [
+      "Wheat and rice",
+      "Wheat ans rice",
+      "Sugarcane",
+      "all of the above"
+    ]
+  },
+  {
+    numb: 14,
+    question: "In which year the current state of Punjab was created",
+    answer: "1947",
+    options: [
+      "1960",
+      "1947",
+      "1947",
+      "1972"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which one is the largest city in Punjab in area wise",
+    answer: "Ludhiana",
+    options: [
+      "Ludhiana",
+      "Amritsar",
+      "Mohali",
+      "Jalandhar"
+    ]
+  },
+  {
+    numb: 16,
+    question: "The Lohri festival is dedicated to this god.",
+    answer: "The god of sun",
+    options: [
+      "The god of Harvest",
+      "The god of moon.",
+      "The god of sun",
+      "The god of rain"
+    ]
+  },
+  {
+    numb: 17,
+    question: "When is Baisakhi celebrated?",
+    answer: "13th April",
+    options: [
+      "1st April",
+      "16th April",
+      "13th April",
+      "23rd April"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which of the dishes are belongs to special Punjabi cuisine",
+    answer: "Rajma chawal",
+    options: [
+      "Kadhi chawal",
+      "Rajma chawal",
+      "RGurwala chawal",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Punjab is the major producer of?",
+    answer: "wheat, rice and dairy products",
+    options: [
+      "corn, wheat and rice",
+      "sugar, bajra and wheat",
+      "soyabean, oats and barley",
+      "wheat, rice and dairy products"
+    ]
+  },
+  {
+    numb: 20,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 21,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 22,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 23,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 24,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 25,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+
+];
